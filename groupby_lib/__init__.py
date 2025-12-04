2b5689aaf02d89581be9457d01aca1bcca3381d0
Eoin Condron <econdr@gmail.com>
2025-12-04 10:11:43 +0000

    Add ema_grouped, a method for calculating group-wise EMAs

--- groupby_lib/__init__.py
+++ groupby_lib/__init__.py
@@ -5,5 +5,6 @@ try:
 except PackageNotFoundError:
     __version__ = "0.0.0+unknown"
 
+from groupby_lib.ema import ema, ema_grouped
 from groupby_lib.groupby import GroupBy, crosstab, install_groupby_fast
 from groupby_lib.util import bools_to_categorical, nb_dot, pretty_cut
